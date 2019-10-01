56aaf7690d4448aad2e9fdb78399499f0cc9c14f
Tin Luong <tinluong28@gmail.com>
2019-09-30 20:24:24 -0700

    first commit

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = "AIzaSyBcW69jfSSAW35qs9fCSWiy44zExIcpNKc";
+
+export default axios.create({
+    baseURL: "https://www.googleapis.com/youtube/v3",
+    params: {
+        part: "snippet",
+        maxResults: 5,
+        key: KEY
+    }
+})
---
+++ src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    state = {
+        term: '',
+    }
+    inputChange = (e) => {
+        this.setState({term: e.target.value })
+    }
+    onFormSubmit = (e) => {
+        e.preventDefault();
+
+    }
+    render(){
+        return(
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form" action="">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <div className="ui search">
+                            <div className="ui icon input">
+                                <input
+                                    onChange={this.inputChange}
+                                    value={this.state.term}
+                                    type="text"
+                                    className="prompt"/>
+                                <i className="search icon"></i>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
